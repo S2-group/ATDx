7f351995af081f96c72aec6d17b0124c5b9f96e9
Sebastian-Ospina <s.ospinacamacho@student.vu.nl>
2021-06-07 11:22:13 +0200

    Skeleton for Mediator Class

---
+++ code/MediatorSonarCloudAtdx.py
@@ -0,0 +1,12 @@
+
+from ClassMediator import *
+
+
+class MediatorSonarCloudAtdx(ClassMediator, ABC):
+
+    def __init__(self, atdx_core, sonar_cloud):
+        self.atdx = atdx_core
+        self.sonar_cloud = sonar_cloud
+
+    def notify(self, sender, event, argument):
+        pass
